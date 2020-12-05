9b7f5c9611aa1ba5d33b67ef838e7b54c6e3506b
Jason Lauman <jason@lauman.net>
2020-12-05 12:23:16 -0700

    changes.

--- template/package/hello3/server.js
+++ template/package/hello3/server.js
@@ -4,18 +4,18 @@ import Express from 'express';
 import Morgan from 'morgan';
 const app = Express();
 const port = 8080;
-const files = [
-  'target/scala-3.0.0-M2/hello3-fastopt/hello3.js',
-  'target/scala-3.0.0-M2/hello3-fastopt/hello3.js.map'
-];
+// const files = [
+//   'target/scala-3.0.0-M2/hello3-fastopt/hello3.js',
+//   'target/scala-3.0.0-M2/hello3-fastopt/hello3.js.map'
+// ];
 
-for (let file of files) {
-  if (!Fs.existsSync(file)) {
-    console.error('no file: ' + file);
-    process.exit(1);
-  }
-  Fs.copyFileSync(file, Path.join('./web', Path.basename(file)));
-}
+// for (let file of files) {
+//   if (!Fs.existsSync(file)) {
+//     console.error('no file: ' + file);
+//     process.exit(1);
+//   }
+//   Fs.copyFileSync(file, Path.join('./web', Path.basename(file)));
+// }
 
 // app.use(Morgan('combined', { stream: Fs.createWriteStream(Path.join('log', 'access.log')) }));
 app.use(Morgan('tiny'));
--- template/package/hello3/src/main/scala/Hello3.scala
+++ template/package/hello3/src/main/scala/Hello3.scala
@@ -1,14 +1,19 @@
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
 
+@js.native
+@JSGlobal
+def breakpoint(debug: Boolean, message: String): Boolean = js.native
+
 @JSExportTopLevel(name = "Hello3", moduleID = "hello3")
 object Hello3 {
 
   @JSExport
-  def hello(name: String): Unit = {
+  def hello(name: String = "UNKNOWN"): Unit = {
     println(s"Hello3: Hello ${name}!")
     var args: List[String] = List("one", "two", "three")
     val count = args.length
+    // breakpoint(true, "what?")
     println(s"Hello3: names=${args.toList}")
     println(s"Hello3: count=${count}")
   }
